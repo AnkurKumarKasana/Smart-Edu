a87e2bed03469f0a13f127ce8b1bba90a2bcce19
Ankurkumarkasana <officialgujar@gmail.com>
2025-04-06 02:20:40 +0530

    "Added CourseDetailActivity and button navigation"

--- app/src/main/java/com/example/instagram/CourseDetailActivity.kt
+++ app/src/main/java/com/example/instagram/CourseDetailActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class CourseDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_course_detail)
+        setContentView(R.layout.activity_course_detail) 
     }
 }
